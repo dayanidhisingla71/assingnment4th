514010eaeb3e154e78ab7d89d61aad4accadacc5
Dayanidhi <38643429+dayanidhisingla71@users.noreply.github.com>
2018-06-15 20:48:07 +0530

    Update Sort.java

--- Assignment4/Sort.java
+++ Assignment4/Sort.java
@@ -1,32 +1,26 @@
-package assignment3;
-import java.util.Scanner;
-public class Sort {
-	public static void main(String args[])
-	{   int temp;
-		int arr[] = new int[8];
-		arr[0]=1;
-		arr[1]=1;
-		arr[2]=1;
-		arr[3]=1;
-		arr[4]=0;
-		arr[5]=0;
-		arr[6]=1;
-		arr[7]=0;
-		
-		for(int i = 0; i < 8 ; i++)
-		{
-			for(int j = 0 ; j < i ; j++)
-			{
-				if(arr[i] < arr[j])
-				{
-					temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
-			}
-		}
-		System.out.println("Sorted Array "+arr[0]+" , "+arr[1]+" , "+arr[2]+" , "+arr[3]+" , "+arr[4]+" , "+arr[5]+" , "+arr[6]+ " , "+arr[7]);
-		
-	}
+//Reverse an array in ascending order
+
+public class Class3_AltSorting_AdityaGkar {
+    public static void main(String args[]){
+
+        int arr[]={1,1,1,1,0,0,1,0};
+        int length=arr.length;
+      int temp;
+
+      int j=0;
+        for(int i=0;i<length;i++)
+        {
+            if(arr[i]==0)
+            {
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+            }
+
+        }
+
+        for(int i=0;i<length;i++)
+            System.out.println(" "+arr[i]);
+    }
 }
-	
